58e860d319bc678800efa0c0ee54d756c4304c99
juancarlos-1983 <57755785+juancarlos-1983@users.noreply.github.com>
2026-04-20 19:53:33 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(
+  'https://eeodyjmlbzwjnfsvlurx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlb2R5am1sYnp3am5mc3ZsdXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTU1MjIsImV4cCI6MjA5MjI5MTUyMn0.dV6GFlGYbN7hyeO0cAifysrIx24Beg08XV5hsB-lWT8'
+);
